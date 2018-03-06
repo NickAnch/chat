4d6a05995d6e698b650524d2779a138d616998d3
NickAnch <kolya_anch@mail,ru>
2018-03-07 02:38:42 +0300

    Публикация

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-	$base = mysqli_connect('localhost','root','1234admin','Chat_database');
+	$base = mysqli_connect('localhost','','','Chat_database');
 	if(mysqli_connect_errno()){
 		echo mysqli_connect_error();
 		exit();
 	}
-    
+
     session_start();
 ?>
